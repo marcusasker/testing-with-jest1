3bc3f6acf7e3088197ed43a9bb65b0cabaf87855
Marcus Asker <asker.marcus@gmail.com>
2023-08-29 16:37:44 +0200

    bad implementation with a callback function

--- src/UserForm.test.js
+++ src/UserForm.test.js
@@ -1,5 +1,5 @@
 import { render, screen } from '@testing-library/react';
-import { user } from '@testing-library/user-event';
+import user from "@testing-library/user-event";
 import UserForm from './UserForm';
 
 test('it shows two inputs and a button', () => {
@@ -15,3 +15,41 @@ test('it shows two inputs and a button', () => {
     expect(inputs).toHaveLength(2);
     expect(button).toBeInTheDocument();
 });
+
+
+test("it calls onUserAdd when the form is submitted", () => {
+    // NOT THE BEST IMPLEMENTATION
+    const { callback, argList } = newFunction();
+    // Try to render my component
+    render(<UserForm onUserAdd={callback} />);
+
+    // Find the two inputs
+    const [nameInput, emailInput] = screen.getAllByRole("textbox");
+
+    // Simulate typing in a name
+    user.click(nameInput);
+    user.keyboard("jane");
+
+    // Simulate typing in an email
+    user.click(emailInput);
+    user.keyboard("jane@jane.com");
+    
+    // Find the button
+    const button = screen.getByRole('button');
+
+    // Simulate clickin the button
+    user.click(button);
+
+    // Assertion to make sure 'onUserAdd' is called with email/name
+    expect(argList).toHaveLength(1);
+    expect(argList[0][0]).toEqual({ name: 'jane', email: 'jane@jane.com'})
+
+
+    function newFunction() {
+        const argList = [];
+        const callback = (...args) => {
+            argList.push(args);
+        };
+        return { callback, argList };
+    }
+})
